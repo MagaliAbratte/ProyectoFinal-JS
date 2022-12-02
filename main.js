da8c5b763a96271a08bc79174122bc1148752798
Magali Abratte <maguiabratte@gmail.com>
2022-12-02 16:45:34 -0300

    edicion de sass y correccion de codigo

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-//DOM, EVENTOS Y STORAGE - E-COMMERCE ESTUDIO CREA
+//SIMULADOR E-COMMERCE ESTUDIO CREA//
+
+//PRODUCTOS//
 
 class ProductosCrea {
     constructor (id,img, nombre, precio, descripcion, cantidad){
@@ -12,43 +14,6 @@ class ProductosCrea {
   }
   
 const productos = [];
- 
-/* let mates1 = new ProductosCrea (1,'MATE CELESTE','./assets/img/sliders/mate1.jpg', 1200, 'Mate de ceramica moldeado a mano, con detalles en relieve y flores celestes.',1);
-productos.push (mates1)
-let mates2 = new ProductosCrea (2, 'MATE LISO','./assets/img/sliders/mates2.jpg', 1200, 'Mate de ceramica moldeado a mano, con detalles en relieve y flores rosadas.',1);
-productos.push (mates2)
-let mates3 = new ProductosCrea (3, 'MATE ROSADO','./assets/img/sliders/mates3.jpg', 1200, 'Mate de ceramica moldeado a mano, con detalles en relieve y color liso.',1);
-productos.push (mates3)
-let almohadones1 = new ProductosCrea (4, 'ALMOHADON BORDADO','./assets/img/sliders/almohadon1.jpg', 1500, 'Almohadon 50cm x 40cm, rayado con apliques sublimados, varios colores.',1);
-productos.push (almohadones1)
-let almohadones2 = new ProductosCrea (5, 'ALMOHADON RAYADO','./assets/img/sliders/almohadon2.jpg', 1500, 'Almohadon 50cm x 40cm, rayado con bordes con apliques, varios colores.',1);
-productos.push (almohadones2)
-let almohadones3 = new ProductosCrea (6, 'ALMOHADON CON BORDE','./assets/img/sliders/almohadon3.jpg', 1500, 'Almohadon 50cm x 40cm, con apliques, bordados y flecos de broderi.',1);
-productos.push (almohadones3)
-let canastos1 = new ProductosCrea (7, 'CANASTO TEJIDO','./assets/img/sliders/canastos1.jpg', 1600, 'Canastos de produccion artesanal, armados con paja verde.',1);
-productos.push (canastos1)
-let canastos2 = new ProductosCrea (8, 'CANASTO BICOLOR','./assets/img/sliders/canastos2.jpg', 1600, 'Canastos de produccion artesanal, armados con paja trenzada.',1);
-productos.push (canastos2)
-let canastos3 = new ProductosCrea (9, 'CANASTO TRENZADO','./assets/img/sliders/canastos3.jpg', 1600, 'Canastos de produccion artesanal, armados con paja de dos colores.',1);
-productos.push (canastos3)
-let plantas1 = new ProductosCrea (10, ' PLANTA MONSTERA','./assets/img/sliders/monstera.jpg', 1800, 'Planta monstera de tamaño mediano o grande. Se entrega lista para transplantar.',1);
-productos.push (plantas1)
-let plantas2 = new ProductosCrea (11, 'PLANTA SCHEFFLERA','./assets/img/sliders/schefflera.jpg', 1800, 'Planta schefflera de tamaño mediano o grande. Se entrega lista para transplantar.',1);
-productos.push (plantas2)
-let plantas3 = new ProductosCrea (12, 'PLANTA CROTON','./assets/img/sliders/croton.jpg', 1800, 'Planta croton de tamaño mediano o grande. Se entrega lista para transplantar.',1);
-productos.push (plantas3)  */ 
-
-  
-document.addEventListener ('keyup', (e)=>{
-  if (e.target.matches("#buscador")){
-    document.querySelectorAll (".tarjetaProducto").forEach (elemento =>{
-      elemento.textContent.toLowerCase().includes (e.target.value.toLowerCase())
-      ? elemento.classList.remove ("filtro")
-      : elemento.classList.add ("filtro")
-    })
-  }
-})
-
 const contenedorProductos = document.getElementById('contenedorProductos');
 
 async function getProductsFromJSON() {
@@ -63,37 +28,50 @@ async function getProductsFromJSON() {
 
 function crearTarjetas () {
   productos.forEach((producto) => {
-      const divProducto = document.createElement('div');
-      divProducto.className = 'tarjetaProducto'
-      divProducto.innerHTML = `
-          <div class='datosTarjeta'>
-          <div class="card-body">
-          <img src="${producto.img}" class= "imagenesProductos">
-          <h3 class="card-title"> ${producto.nombre} </h3>
-          <p class='card-text-descripcion'>${producto.descripcion}</p>
-          <p class="card-text-precio"> $${producto.precio} </p>
-          <button id="boton${producto.id}" class="boton">Añadir</button>
-          </div>
-          </div>`;
-  
-      contenedorProductos.append(divProducto);
-  
-      const boton = document.getElementById(`boton${producto.id}`);
-      boton.addEventListener('click', () => {
-        agregarAlCarrito(producto.id);
-      });
+    const divProducto = document.createElement('div');
+    divProducto.className = 'tarjetaProducto'
+    divProducto.innerHTML = `
+    <div class='datosTarjeta'>
+    <div class="card-body">
+    <img src="${producto.img}" class= "imagenesProductos">
+    <h3 class="card-title"> ${producto.nombre} </h3>
+    <p class='card-text-descripcion'>${producto.descripcion}</p>
+    <p class="card-text-precio"> $${producto.precio} </p>
+    <button id="boton${producto.id}" class="boton">Añadir</button>
+    </div>
+    </div>`;
+    
+    contenedorProductos.append(divProducto);
+    
+    const boton = document.getElementById(`boton${producto.id}`);
+    boton.addEventListener('click', () => {
+      agregarAlCarrito(producto.id);
     });
+  });
 }
-  
-  const carrito = [];
-  const contenedorCarrito = document.getElementById('contenedorCarrito');
-  const contador = document.getElementById ('contador')
+
+//BUSCADOR//
+
+document.addEventListener ('keyup', (e)=>{
+  if (e.target.matches("#buscador")){
+    document.querySelectorAll (".tarjetaProducto").forEach (elemento =>{
+      elemento.textContent.toLowerCase().includes (e.target.value.toLowerCase())
+      ? elemento.classList.remove ("filtro")
+      : elemento.classList.add ("filtro")
+    })
+  }
+})
+
+//CARRITO//
+
+const carrito = [];
+const contenedorCarrito = document.getElementById('contenedorCarrito');
+const contador = document.getElementById ('contador')
   
   const contadorCarrito = ()=>{
   contador.style.display = 'block'
   contador.innerText = carrito.length
   }
-  
   
   const agregarAlCarrito = (id) => {
     const producto = productos.find((producto) => producto.id === id);
@@ -108,7 +86,7 @@ function crearTarjetas () {
   };
 
 
-function actualizarCarrito() {
+  function actualizarCarrito() {
     let actualizar = '';
     carrito.forEach((producto) => {
         actualizar += `
@@ -122,9 +100,9 @@ function actualizarCarrito() {
     
   contenedorCarrito.innerHTML = actualizar;
   calcularTotalCompra();
-}
+ }
 
-const eliminarDelCarrito = (id) => {
+  const eliminarDelCarrito = (id) => {
     const producto = carrito.find((producto) => producto.id === id);
     carrito.splice(carrito.indexOf(producto), 1);
     actualizarCarrito();
@@ -140,9 +118,10 @@ const eliminarDelCarrito = (id) => {
   
   const totalCompra = document.getElementById('totalCompra');
   const completar = document.getElementById ('completarCompra')
+  let total
   
   const calcularTotalCompra = () => {
-    let total = 0
+    total = 0
     carrito.forEach((producto) => {
       total += producto.precio * producto.cantidad;
     });
@@ -153,19 +132,9 @@ const eliminarDelCarrito = (id) => {
     }
   };
 
-  const datosIngresados = {}
+  //DATOS DE USUARIO//
 
- /*  if (datosIngresados){
-    Swal.fire ({
-      title: `Bienvenidx ${JSON.parse(localStorage.getItem("datosCliente")).nombre}`,
-      text: `Nos alegramos de tenerte de vuelta`,
-      timer: 2500,
-      showConfirmButton: false,
-      background: '#D9D9D9',
-      width: '550px',
-      padding: '50px',
-    })
-  } */ 
+  const datosIngresados = {}
 
   function completarForm (){
     let datosUsuario = document.createElement ('div')
@@ -176,7 +145,7 @@ const eliminarDelCarrito = (id) => {
       <input id="apellidoUsuario" class= 'inputDatos' placeholder='Ingresa tu apellido'></input>
       <input id="telefonoUsuario"  class= 'inputDatos' placeholder='Ingresa tu telefono'></input>
       <input id="correoUsuario" class= 'inputDatos' placeholder='Ingresa tu correo electronico'></input>
-      <input id="direccionUsuario" class= 'inputDatos' placeholder='Ingresa tu direccion'></input>
+      <input id="direccionUsuario" class= 'inputDatos' placeholder='Ingresa tu direccion - Localidad - Provincia'></input>
       <button id="botonEnviarDatos" class='boton'>Enviar</button>
       `
       carritoDeCompras.append (datosUsuario); 
@@ -213,9 +182,11 @@ const eliminarDelCarrito = (id) => {
         let divFinal = document.createElement ('div')
         divFinal.className = 'div-final'
         divFinal.innerHTML = `
-        <p class= 'texto'>¡Gracias por tu compra ${JSON.parse(localStorage.getItem("datosCliente")).nombre}!</p>
-        <p class= 'texto'>Enviaremos tu pedido a ${JSON.parse(localStorage.getItem("datosCliente")).direccion}.
-        Recibiras en tu correo electronico las indicaciones para realizar el pago.
+        <h2 class= 'texto'>¡Gracias por tu compra ${JSON.parse(localStorage.getItem("datosCliente")).nombre}!</h2>
+        <p class= 'texto'>Esta es la informacion de tu compra:</p>
+        <p class= 'texto'>Total a pagar: $${total}</p>
+        <p class= 'texto'>Recibiras en tu correo electronico las indicaciones para realizar el pago.
+        Una vez realizado el mismo, enviaremos tu pedido a ${JSON.parse(localStorage.getItem("datosCliente")).direccion}.
         En las siguientes 48 hs enviaremos el codigo de seguimiento de tu pedido a tu correo electronico.
         Ante cualquier inconveniente, comunicate a 3563487569 o estudiocrea@gmail.com.
         </p>
@@ -226,7 +197,5 @@ const eliminarDelCarrito = (id) => {
     })
   } 
 
-  getProductsFromJSON()
+getProductsFromJSON()
 
-   
-/*   <img src="${producto.img}" class= "imagenesProductos"> */
